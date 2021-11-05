221d2680b8bdad2aaff7a6fe230d66bc22043bed
Rhys Malyon <malyonr@gmail.com>
2021-11-05 13:14:18 +0900

    Fix routes and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'places/index'
-  get 'places/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'places#index'
+  resources :places, only: %i[index show]
 end
